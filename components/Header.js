1dd18b0dd7ba5ace9a221e03b302d55ba443197c
letxx <115582233+leletns@users.noreply.github.com>
2026-03-27 23:09:48 -0300

    Adicionar componente Header

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="bg-white dark:bg-gray-800 p-4 shadow">
+            <h1 className="text-xl font-bold text-gray-900 dark:text-white">Título do Site</h1>
+        </header>
+    );
+};
+
+export default Header;
